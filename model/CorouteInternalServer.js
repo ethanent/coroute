9b9c0f521df9bcdd56dd39675ffc331b6beb3563
Ethan Davis <futuristiccake@gmail.com>
2018-02-23 21:36:43 -0800

    1.0.0 prep: Major bugfixes!

--- model/CorouteInternalServer.js
+++ model/CorouteInternalServer.js
@@ -4,7 +4,7 @@ const https = require('https')
 const util = require('util')
 const fs = require('fs')
 const pfs = {
-	'readFileSync': util.promisify(fs.readFileSync)
+	'readFile': util.promisify(fs.readFile)
 }
 const V = require('vaxic')
 
@@ -18,51 +18,60 @@ module.exports = class CorouteInternalServer {
 
 		this.app.use(V.route)
 
-		this.app.add(this.coreHandler)
+		this.app.add(async (req, res) => {
+			const useRoute = this.routes.find((route) => {
+				if (route.from.hasOwnProperty('server') && route.from.server !== this.name) {
+					return false
+				}
+
+				if (route.from.hasOwnProperty('host') && req.headers.hasOwnProperty('host') && route.from.host.toLowerCase() !== req.headers.host.toLowerCase()) {
+					return false
+				}
+
+				if (route.from.hasOwnProperty('method') && route.method !== req.method) {
+					return false
+				}
+
+				return true
+			})
+
+			if (useRoute) {
+				try {
+					await res.route({
+						'origin': useRoute.to
+					})
+				}
+				catch (err) {
+					console.error('Routing failed: ' + err)
+					res.writeHead(500)
+					res.end('Coroute 500: Routing failed.')
+				}
+			}
+			else {
+				res.writeHead(404)
+				res.end('Coroute 404: No route found')
+			}
+		})
 
 		this.startServer().then(() => {
 			console.log('Server ' + name + ' has started.')
 		}).catch((err) => {
-			console.error('Server ' + name + 'failed to start: ' + err)
+			console.error('Server ' + name + ' failed to start: ' + err)
 		})
-	}
-
-	coreHandler (req, res) {
-		const useRoute = routes.find((route) => {
-			if (route.from.hasOwnProperty('server') && route.from.server !== this.name) {
-				return false
-			}
-
-			if (route.from.hasOwnProperty('host') && req.headers.hasOwnProperty('host') && route.from.host.toLowerCase() !== req.headers.host.toLowerCase()) {
-				return false
-			}
-
-			return true
-		})
-
-		if (useRoute) {
-			res.route({
-				'origin': useRoute.to
-			})
-		}
-		else {
-			res.writeHead(404)
-			res.end('Coroute 404: No route found')
-		}
 	}
 
 	async startServer () {
 		if (this.options.hasOwnProperty('https')) {
 			this.internalServer = https.createServer({
-				'key': await pfs.readFileSync(path.resolve(process.cwd(), options.https.key)),
-				'cert': await pfs.readFileSync(path.resolve(process.cwd(), options.https.cert))
+				'key': await pfs.readFile(path.resolve(process.cwd(), this.options.https.key)),
+				'cert': await pfs.readFile(path.resolve(process.cwd(), this.options.https.cert))
 			}, this.app.serverHandler)
 		}
 		else {
 			this.internalServer = http.createServer(this.app.serverHandler)
 		}
 
-		this.app.serverHandler.listen(options.port, options.host, () => {
+		this.app.internalServer.listen(this.options.port, this.options.host, () => {
 			return
 		})
 	}
